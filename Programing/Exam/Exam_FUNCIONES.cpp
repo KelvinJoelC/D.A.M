beca68feff1d1c1e9b15f8534d5276888a0f3435
KelvinJoelC <Kelvinjcarreno@gmail.com>
2018-03-15 18:13:48 +0100

    For,Bucles,Funciones,Punteros

--- Programing/Exam/Exam_FUNCIONES.cpp
+++ Programing/Exam/Exam_FUNCIONES.cpp
@@ -1,13 +1,14 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-
+/*Indicamos las funciones existentes*/
 void leer(int *dato1,int *dato2);
 
 int main(){
+	/*Inicializamos las variables donde se van a guardar*/
 	int CualquierCosa1, 
 	    CualquierCosa2;
-	
+	/*Llamamos a la funcion leer*/
 	leer(&CualquierCosa1,&CualquierCosa2); 
     	printf("%i\n", CualquierCosa1);
 	printf("%i\n", CualquierCosa2);
@@ -16,11 +17,14 @@ int main(){
     return EXIT_SUCCESS;
     
 }
-
+/*Creamos las funcion leer*/
+/*Esperamos que nos devuelva lo que hay allí donde apunta dato1 y dato2*/
 void leer(int *dato1, int *dato2){
 	
 	printf("Introduce el primer numero: ");
 	scanf(" %i", dato1);
+	/*Guardamos un entero en lo que hay en dato1*/
 	printf("Introduce el segundo numero: ");
+	/*Guardamos un entero en lo que hay en dato1*/
 	scanf(" %i", dato2);
 }
--- Programing/Exam/Exam_PUNTEROS.cpp
+++ Programing/Exam/Exam_PUNTEROS.cpp
@@ -1,6 +1,8 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+
+
 /*Crea una reserva de memoria para que quepa un double.
   Guarda 3.5 en dicha reserva
   Amplia para que quepa 2 double.
@@ -8,12 +10,21 @@
 int main(){
 
 	double *reserva = NULL;
-
+    /*1º creamos una reserva de memoria con al tamaño de un double te devuelve un 
+     la dirección de memoria de un double */
     reserva =(double *)malloc(sizeof(double));
+    /*reserva pasa a valer una dirección de memoria donde cabe un double*/
     *reserva = 3.5;
+    /*"Lo que hay allí donde apunta reserva(cualquier cosa) pasa a valer 3.5"*/
+    /*Ampliamos reserva para que no quepa un double, sino 2 doubles*/
     reserva = (double *)realloc(reserva, 2 * sizeof(double));
+    /*Lo que vale reserva =             |<-dos doubles  ->|*/
     *(reserva+1)= 7.8;	
+    /*Lo que hay alli donde apunta reserva +1 pasa a valer 7.8
+      reserva valía 3.5 pero lo ampliamos y valía 3.5+un espacio vacio donde cabe un
+      double. Finalmente reserva vale 3.57.8*/
     free(reserva);
+    /*Sirve para liberar la memoria*/
 
 
     return EXIT_SUCCESS;
